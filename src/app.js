293f7f459e53a728691e47ffa0f7d4543d5f3af3
3rd-Web <web_3@icloud.com>
2022-05-27 04:00:22 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["dog", "spy", "ant", "cow", "Uber driver", "alien"];
+  let action = [
+    "stole my",
+    "hid my",
+    "ran away with my",
+    "laughed at my",
+    "ate my"
+  ];
+  let item = ["keys", "beer", "phone", "computer", "cousin"];
+  let where = [
+    "at the pool",
+    "in my grandmother's house",
+    "at the club",
+    "in the bathroom",
+    "in Mexico"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let itemIndex = Math.floor(Math.random() * item.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    item[itemIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
